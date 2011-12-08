0f241d97f303f9a407247fc107f98e0693319c9a
jean-philippe serafin <serafinjp@gmail.com>
2011-12-08 08:33:22 +0100

    Removed a debug line

--- static/site.js
+++ static/site.js
@@ -2,6 +2,5 @@
     var codes = document.getElementsByTagName('code');
     for (var i = 0, len = codes.length; i < len; i++) {
         hljs.highlightBlock(codes[i]);
-        console.log(codes[i]);
     }
 })();
