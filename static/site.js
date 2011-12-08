933b380e1ac3f9021d0daa12a95252708995d66c
jean-philippe serafin <serafinjp@gmail.com>
2011-12-08 07:39:36 +0100

    Moved intrusive scripts

---
+++ static/site.js
@@ -0,0 +1,7 @@
+(function() {
+    var codes = document.getElementsByTagName('code');
+    for (var i = 0, len = codes.length; i < len; i++) {
+        hljs.highlightBlock(codes[i]);
+        console.log(codes[i]);
+    }
+})();
