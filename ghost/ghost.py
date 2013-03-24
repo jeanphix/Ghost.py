8b833bc4ecd8e4bc55f46251a068826d44cef232
Arik Fraimovich <arik@arikfr.com>
2013-03-24 19:35:27 +0200

    Fix: if final URL contains hash, no page contents will be returned.

--- ghost/ghost.py
+++ ghost/ghost.py
@@ -745,6 +745,7 @@ class Ghost(object):
         page = None
 
         url = self.main_frame.url().toString()
+        url = url.split("#")[0]
 
         for resource in resources:
             if url == resource.url:
