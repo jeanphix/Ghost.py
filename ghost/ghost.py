90894895fcaf6c77ae27a6752ae5a2892889930a
Gustavo Carneiro <gjc@gambitresearch.com>
2013-05-07 11:34:51 +0100

    Python modules should call logging.basicConfig()
    Only applications should call logging.basicConfig().

--- ghost/ghost.py
+++ ghost/ghost.py
@@ -41,7 +41,6 @@ default_user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.2 " +\
     "(KHTML, like Gecko) Chrome/15.0.874.121 Safari/535.2"
 
 
-logging.basicConfig()
 logger = logging.getLogger('ghost')
 
 
