09cb312f005e5af1807f71dd15d3df5993412d7d
jean-philippe serafin <serafinjp@gmail.com>
2013-12-23 16:03:05 +0100

    Fixes HttpResource pep8

--- ghost/ghost.py
+++ ghost/ghost.py
@@ -190,11 +190,16 @@ class HttpResource(object):
         Logger.log("Resource loaded: %s %s" % (self.url, self.http_status))
         self.headers = {}
         for header in reply.rawHeaderList():
-	    try:
-            	self.headers[unicode(header)] = unicode(reply.rawHeader(header))
-	    except UnicodeDecodeError:
-	    	# it will lose the header value, but at least not crash the whole process
-	    	logger.error("Invalid characters in header {0}={1}".format(header,reply.rawHeader(header)))
+            try:
+                self.headers[unicode(header)] = unicode(
+                    reply.rawHeader(header))
+            except UnicodeDecodeError:
+                # it will lose the header value,
+                # but at least not crash the whole process
+                logger.error(
+                    "Invalid characters in header {0}={1}"
+                    % (header, reply.rawHeader(header))
+                )
         self._reply = reply
 
 
